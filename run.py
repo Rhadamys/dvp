5049af8836f0a74c90a4a3d9f1928eef0c5f9389
Mario Álvarez Molina <mario.alvarez.m@usach.cl>
2019-01-13 21:27:19 +0000

    Host Loader.io permitido

--- run.py
+++ run.py
@@ -14,20 +14,25 @@ class TimeOutException(Exception):
     pass
 
 @app.route('/api/trace', methods=['POST'])
-@cross_origin(origins=['https://rhadamys.pythonanywhere.com', 'http://localhost:8080'])
+@cross_origin(origins=['https://rhadamys.pythonanywhere.com', 'http://localhost:8080', 'https://loader.io'])
 def request_trace():
     data = request.get_json(force=True)
     try:
         trace = generate_trace(data)
     except TimeOutException:
         exception = dict(event='instruction_limit_reached',
-                         exception_msg='''Tu programa ha estado operando por <u>más de {0} segundos</u> 
-                         y se ha cancelado su ejecución. Esto puede ocurrir si realizas operaciones con 
-                         valores muy grandes (Por ejemplo, calcular el <u>factorial de 1000</u>. ¡Qué 
+                         exception_msg='''Tu programa ha estado operando por <u>más de {0} segundos</u>
+                         y se ha cancelado su ejecución. Esto puede ocurrir si realizas operaciones con
+                         valores muy grandes (Por ejemplo, calcular el <u>factorial de 1000</u>. ¡Qué
                          locura!).'''.format(MAX_TIME_RUNNING),
                          limit='time')
         trace = jsonify([exception])
     return trace
+
+@app.route('/loaderio-d303c47fb9a2d571dd01fa4f9d042c65/')
+@cross_origin(origins=['https://loader.io'])
+def loader_io():
+    return 'loaderio-d303c47fb9a2d571dd01fa4f9d042c65'
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
